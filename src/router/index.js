b38272a764a9001aaa16ea712d8255ceb04e82d6
binhantt <doan4503@gmail.com>
2024-12-05 17:57:06 +0700

    'save'

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import { createRouter, createWebHistory } from 'vue-router';
         path : "/home", 
         name : 'home', 
         component :()=> import('../views/HomeView.vue')
-        
     }
  ];
  const router = createRouter({
